7c93291e1c5e86a105be6b905e488919dad5d9ed
boomeraang <sooryankarthick@gmail.com>
2017-10-31 18:45:44 +0530

    removed build.gradle

---
+++ src/main/java/city/CopDrone.java
@@ -0,0 +1,9 @@
+package city;
+
+/**
+ * Created by cybereagle3-1 on 28/10/17.
+ */
+
+public class CopDrone extends Drone
+{
+}
---
+++ src/main/java/users/Admin.java
@@ -0,0 +1,9 @@
+package users;
+
+/**
+ * Created by cybereagle3-1 on 30/10/17.
+ */
+
+public class Admin extends User
+{
+}
